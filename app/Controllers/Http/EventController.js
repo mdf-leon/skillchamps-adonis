aebc928f3466bdbdacefb46fa22c08904217fce9
Matheus DF <51722889+matheusdf32@users.noreply.github.com>
2021-03-02 12:50:18 -0300

    Update EventController.js

--- app/Controllers/Http/EventController.js
+++ app/Controllers/Http/EventController.js
@@ -435,10 +435,8 @@ class EventController {
     const { event_id } = params
     const photo_event = request.file('photo_event')
     const photo_folder = request.file('photo_folder')
-    if (photo_event?.tmpPath)
-      const photo_event_entity = await Image.create({ b64: photo_event && fs.readFileSync(photo_event.tmpPath, { encoding: 'base64' }) })
-    if (photo_folder?.tmpPath)
-      const photo_folder_entity = await Image.create({ b64: photo_folder && fs.readFileSync(photo_folder.tmpPath, { encoding: 'base64' }) })
+    const photo_event_entity = photo_event?.tmpPath && await Image.create({ b64: photo_event && fs.readFileSync(photo_event.tmpPath, { encoding: 'base64' }) })
+    const photo_folder_entity = photo_folder?.tmpPath && await Image.create({ b64: photo_folder && fs.readFileSync(photo_folder.tmpPath, { encoding: 'base64' }) })
     const eventData = request.only([
       'event_name', //event name
       'date_begin',
