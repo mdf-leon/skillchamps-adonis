469ec54b4b02486b4ede7307879ca9f3e7407a25
Matheus DF <51722889+matheusdf32@users.noreply.github.com>
2021-10-19 17:49:05 -0300

    Update ScoreController.js

--- app/Controllers/Http/ScoreController.js
+++ app/Controllers/Http/ScoreController.js
@@ -1,291 +1,347 @@
-'use strict'
+"use strict";
 
-const User = use('App/Models/User')
-const Rider = use('App/Models/Rider')
-const Institute = use('App/Models/Institute')
-const Entity = use('App/Models/Entity')
-const Event = use('App/Models/Event')
-const Trial = use('App/Models/Trial')
-const PenaltyConf = use('App/Models/PenaltyConf')
-const BonusConf = use('App/Models/BonusConf')
-const Score = use('App/Models/Score')
-const Penalty = use('App/Models/Penalty')
-const Bonus = use('App/Models/Bonus')
+const User = use("App/Models/User");
+const Rider = use("App/Models/Rider");
+const Institute = use("App/Models/Institute");
+const Entity = use("App/Models/Entity");
+const Event = use("App/Models/Event");
+const Trial = use("App/Models/Trial");
+const PenaltyConf = use("App/Models/PenaltyConf");
+const BonusConf = use("App/Models/BonusConf");
+const Score = use("App/Models/Score");
+const Penalty = use("App/Models/Penalty");
+const Bonus = use("App/Models/Bonus");
 //const EntityTag = use('App/Models/EntityTag')
-const Database = use('Database')
-var _ = require('lodash');
+const Database = use("Database");
+var _ = require("lodash");
 
 class ScoreController {
-
-
   async show({ request, response, auth, params }) {
-    let score = await Score.query().where({ id: params.id })
-      .with('penalties.penaltyConf')
-      .with('bonuses.bonusConf')
+    let qsearch = { id: params.id };
+    if (params.id == "new")
+      qsearch = { trial_id: request.trial_id, rider_id: request.rider_id }; // remover esta gambi
+    let score = await Score.query()
+      .where(qsearch)
+      .with("penalties.penaltyConf")
+      .with("bonuses.bonusConf")
       .first();
     if (!score) {
-      return response.status(400).json({ bad_request: 'Score nao existe' })
+      return response.status(400).json({ bad_request: "Score nao existe" });
     }
-    return score.toJSON()
+    return score.toJSON();
   }
 
   async destroy({ request, response, auth, params }) {
-
-    let old = await Score.query().where({ id: params.id })
-      .with('penalties')
-      .with('bonuses')
+    let old = await Score.query()
+      .where({ id: params.id })
+      .with("penalties")
+      .with("bonuses")
       .first();
     if (old) {
       for (let penalty of old.toJSON().penalties) {
-        const pen = await Penalty.findOrFail(penalty.id)
-        await pen.delete()
+        const pen = await Penalty.findOrFail(penalty.id);
+        await pen.delete();
       }
       for (let bonus of old.toJSON().bonuses) {
-        const bon = await Bonus.findOrFail(bonus.id)
-        await bon.delete()
+        const bon = await Bonus.findOrFail(bonus.id);
+        await bon.delete();
       }
-      await old.delete()
+      await old.delete();
+    } else {
+      return response
+        .status(400)
+        .json({ not_found: "could not found this score in DB" });
     }
-    else {
-      return response.status(400).json({not_found: 'could not found this score in DB'})
-    }
-    return response.json({deleted: 'successfully'})
+    return response.json({ deleted: "successfully" });
   }
 
   async index({ request, response, auth, params }) {
-    const { trial_id, rider_id } = request.get()
-    let whereQuery = { trial_id }
-    let score
+    const { trial_id, rider_id } = request.get();
+    let whereQuery = { trial_id };
+    let score;
     if (trial_id || rider_id) {
-      score = await Score.query().where(whereQuery)
-        .with('penalties.penaltyConf')
-        .with('bonuses.bonusConf')
+      score = await Score.query()
+        .where(whereQuery)
+        .with("penalties.penaltyConf")
+        .with("bonuses.bonusConf")
         .fetch();
     } else {
       score = await Score.query()
-        .with('penalties.penaltyConf')
-        .with('bonuses.bonusConf')
+        .with("penalties.penaltyConf")
+        .with("bonuses.bonusConf")
         .fetch();
     }
 
-
     if (!score) {
-      return response.status(400).json({ bad_request: 'Score nao existe' })
+      return response.status(400).json({ bad_request: "Score nao existe" });
     }
-    return score.toJSON()
+    return score.toJSON();
   }
 
-
-  async store({ request, response, auth }) { // to create a score
+  async store({ request, response, auth }) {
+    // to create a score
     const data = request.only([
-      'rider_id',
-      'trial_id',
-      'penalties',
-      'bonuses',
-      'time',
+      "rider_id",
+      "trial_id",
+      "penalties",
+      "bonuses",
+      "time",
     ]);
     //um rider so pode receber a pontuaçao se o gerente estiver dando pontuaçao a um rider inscrito em seu evento
     const rider = await Rider.query()
-      .where('id', '=', data.rider_id)
-      .with('events')
-      .first()
-    if (!rider) return response.status(400).json({ bad_request: 'este rider nao existe' })
+      .where("id", "=", data.rider_id)
+      .with("events")
+      .first();
+    if (!rider)
+      return response
+        .status(400)
+        .json({ bad_request: "este rider nao existe" });
 
     const trial = await Trial.query()
-      .where('id', '=', data.trial_id)
-      .with('event')
-      .first() // trial.toJSON().event.id
-    if (!trial) return response.status(400).json({ bad_request: 'este trial nao existe' })
+      .where("id", "=", data.trial_id)
+      .with("event")
+      .first(); // trial.toJSON().event.id
+    if (!trial)
+      return response
+        .status(400)
+        .json({ bad_request: "este trial nao existe" });
 
     //que essa linha significa que este rider esta inscrito neste evento
-    const event = await rider.events().where('events.id', '=', trial.toJSON().event.id).first()
-    if (!event) return response.status(400).json({ bad_request: 'este rider esta inscrito neste evento?' })
+    const event = await rider
+      .events()
+      .where("events.id", "=", trial.toJSON().event.id)
+      .first();
+    if (!event)
+      return response
+        .status(400)
+        .json({ bad_request: "este rider esta inscrito neste evento?" });
 
-    let old = await Score.query().where({ rider_id: data.rider_id, trial_id: data.trial_id })
-      .with('penalties')
-      .with('bonuses')
+    let old = await Score.query()
+      .where({ rider_id: data.rider_id, trial_id: data.trial_id })
+      .with("penalties")
+      .with("bonuses")
       .first();
     if (old) {
-      return response.status(400).json({ bad_request: 'Ja existe um score' })
+      return response.status(400).json({ bad_request: "Ja existe um score" });
     }
     let res = await Score.create({
-      rider_id: data.rider_id, trial_id: data.trial_id, time: data.time
-    })
+      rider_id: data.rider_id,
+      trial_id: data.trial_id,
+      time: data.time,
+    });
 
-    let pens = []
-    let penaltyTime = 0
+    let pens = [];
+    let penaltyTime = 0;
 
     for (let penalty of data.penalties) {
-      pens.push(await Penalty.create({ ...penalty, score_id: res.id }))
-      const pc = await PenaltyConf.findOrFail(penalty.penalty_conf_id)
-      penaltyTime += (penalty.quantity || 0) * pc.time_penalty
+      pens.push(await Penalty.create({ ...penalty, score_id: res.id }));
+      const pc = await PenaltyConf.findOrFail(penalty.penalty_conf_id);
+      penaltyTime += (penalty.quantity || 0) * pc.time_penalty;
     }
 
-    let bons = []
-    let bonusTime = 0
+    let bons = [];
+    let bonusTime = 0;
 
     for (let bonus of data.bonuses) {
-      bons.push(await Bonus.create({ ...bonus, score_id: res.id }))
-      const bc = await BonusConf.findOrFail(bonus.bonus_conf_id)
-      bonusTime += (bonus.quantity || 0) * bc.time_bonus
+      bons.push(await Bonus.create({ ...bonus, score_id: res.id }));
+      const bc = await BonusConf.findOrFail(bonus.bonus_conf_id);
+      bonusTime += (bonus.quantity || 0) * bc.time_bonus;
     }
 
-    res.time_total = Number(res.time) + Number(penaltyTime) - Number(bonusTime)
-    await res.save()
+    res.time_total = Number(res.time) + Number(penaltyTime) - Number(bonusTime);
+    await res.save();
 
-    res = res.toJSON()
+    res = res.toJSON();
     return response.json({
-      ...res, penaltyTime, bonusTime,
-      penalties: [...pens], bonuses: [...bons]
-    })
+      ...res,
+      penaltyTime,
+      bonusTime,
+      penalties: [...pens],
+      bonuses: [...bons],
+    });
   }
-
 
   async update({ request, response, auth, params }) {
     const data = request.only([
-      'rider_id',
-      'trial_id',
-      'penalties',
-      'bonuses',
-      'time',
+      "rider_id",
+      "trial_id",
+      "penalties",
+      "bonuses",
+      "time",
     ]);
     //um rider so pode receber a pontuaçao se o gerente estiver dando pontuaçao a um rider inscrito em seu evento
     const rider = await Rider.query()
-      .where('id', '=', data.rider_id)
-      .with('events')
-      .first()
-    if (!rider) return response.status(400).json({ bad_request: 'este rider nao existe' })
+      .where("id", "=", data.rider_id)
+      .with("events")
+      .first();
+    if (!rider)
+      return response
+        .status(400)
+        .json({ bad_request: "este rider nao existe" });
 
     const trial = await Trial.query()
-      .where('id', '=', data.trial_id)
-      .with('event')
-      .first() // trial.toJSON().event.id
-    if (!trial) return response.status(400).json({ bad_request: 'este trial nao existe' })
+      .where("id", "=", data.trial_id)
+      .with("event")
+      .first(); // trial.toJSON().event.id
+    if (!trial)
+      return response
+        .status(400)
+        .json({ bad_request: "este trial nao existe" });
 
     //que essa linha significa que este rider esta inscrito neste evento
-    const event = await rider.events().where('events.id', '=', trial.toJSON().event.id).first()
-    if (!event) return response.status(400).json({ bad_request: 'este rider esta inscrito neste evento?' })
+    const event = await rider
+      .events()
+      .where("events.id", "=", trial.toJSON().event.id)
+      .first();
+    if (!event)
+      return response
+        .status(400)
+        .json({ bad_request: "este rider esta inscrito neste evento?" });
 
-    let old = await Score.query().where({ rider_id: data.rider_id, trial_id: data.trial_id })
-      .with('penalties')
-      .with('bonuses')
+    let old = await Score.query()
+      .where({ rider_id: data.rider_id, trial_id: data.trial_id })
+      .with("penalties")
+      .with("bonuses")
       .first();
     if (old) {
       for (let penalty of old.toJSON().penalties) {
-        const pen = await Penalty.findOrFail(penalty.id)
-        await pen.delete()
+        const pen = await Penalty.findOrFail(penalty.id);
+        await pen.delete();
       }
       for (let bonus of old.toJSON().bonuses) {
-        const bon = await Bonus.findOrFail(bonus.id)
-        await bon.delete()
+        const bon = await Bonus.findOrFail(bonus.id);
+        await bon.delete();
       }
-      await old.delete()
+      await old.delete();
     }
     let res = await Score.create({
-      rider_id: data.rider_id, trial_id: data.trial_id, time: data.time
-    })
+      rider_id: data.rider_id,
+      trial_id: data.trial_id,
+      time: data.time,
+    });
 
-    let pens = []
-    let penaltyTime = 0
+    let pens = [];
+    let penaltyTime = 0;
 
     for (let penalty of data.penalties) {
-      pens.push(await Penalty.create({ ...penalty, score_id: res.id }))
-      const pc = await PenaltyConf.findOrFail(penalty.penalty_conf_id)
-      penaltyTime += (penalty.quantity || 0) * pc.time_penalty
+      pens.push(await Penalty.create({ ...penalty, score_id: res.id }));
+      const pc = await PenaltyConf.findOrFail(penalty.penalty_conf_id);
+      penaltyTime += (penalty.quantity || 0) * pc.time_penalty;
     }
 
-    let bons = []
-    let bonusTime = 0
+    let bons = [];
+    let bonusTime = 0;
 
     for (let bonus of data.bonuses) {
-      bons.push(await Bonus.create({ ...bonus, score_id: res.id }))
-      const bc = await BonusConf.findOrFail(bonus.bonus_conf_id)
-      bonusTime += (bonus.quantity || 0) * bc.time_bonus
+      bons.push(await Bonus.create({ ...bonus, score_id: res.id }));
+      const bc = await BonusConf.findOrFail(bonus.bonus_conf_id);
+      bonusTime += (bonus.quantity || 0) * bc.time_bonus;
     }
 
-    res.time_total = Number(res.time) + Number(penaltyTime) - Number(bonusTime) 
-    await res.save()
+    res.time_total = Number(res.time) + Number(penaltyTime) - Number(bonusTime);
+    await res.save();
 
-    res = res.toJSON()
+    res = res.toJSON();
     return response.json({
       updated: true,
-      ...res, penaltyTime, bonusTime,
-      penalties: [...pens], bonuses: [...bons]
-    })
+      ...res,
+      penaltyTime,
+      bonusTime,
+      penalties: [...pens],
+      bonuses: [...bons],
+    });
   }
 
   async replace({ request, response, auth }) {
     const data = request.only([
-      'rider_id',
-      'trial_id',
-      'penalties',
-      'bonuses',
-      'time',
+      "rider_id",
+      "trial_id",
+      "penalties",
+      "bonuses",
+      "time",
     ]);
     //um rider so pode receber a pontuaçao se o gerente estiver dando pontuaçao a um rider inscrito em seu evento
     const rider = await Rider.query()
-      .where('id', '=', data.rider_id)
-      .with('events')
-      .first()
-    if (!rider) return response.status(400).json({ bad_request: 'este rider nao existe' })
+      .where("id", "=", data.rider_id)
+      .with("events")
+      .first();
+    if (!rider)
+      return response
+        .status(400)
+        .json({ bad_request: "este rider nao existe" });
 
     const trial = await Trial.query()
-      .where('id', '=', data.trial_id)
-      .with('event')
-      .first() // trial.toJSON().event.id
-    if (!trial) return response.status(400).json({ bad_request: 'este trial nao existe' })
+      .where("id", "=", data.trial_id)
+      .with("event")
+      .first(); // trial.toJSON().event.id
+    if (!trial)
+      return response
+        .status(400)
+        .json({ bad_request: "este trial nao existe" });
 
     //que essa linha significa que este rider esta inscrito neste evento
-    const event = await rider.events().where('events.id', '=', trial.toJSON().event.id).first()
-    if (!event) return response.status(400).json({ bad_request: 'este rider esta inscrito neste evento?' })
+    const event = await rider
+      .events()
+      .where("events.id", "=", trial.toJSON().event.id)
+      .first();
+    if (!event)
+      return response
+        .status(400)
+        .json({ bad_request: "este rider esta inscrito neste evento?" });
 
-    let old = await Score.query().where({ rider_id: data.rider_id, trial_id: data.trial_id })
-      .with('penalties')
-      .with('bonuses')
+    let old = await Score.query()
+      .where({ rider_id: data.rider_id, trial_id: data.trial_id })
+      .with("penalties")
+      .with("bonuses")
       .first();
     if (old) {
       for (let penalty of old.toJSON().penalties) {
-        const pen = await Penalty.findOrFail(penalty.id)
-        await pen.delete()
+        const pen = await Penalty.findOrFail(penalty.id);
+        await pen.delete();
       }
       for (let bonus of old.toJSON().bonuses) {
-        const bon = await Bonus.findOrFail(bonus.id)
-        await bon.delete()
+        const bon = await Bonus.findOrFail(bonus.id);
+        await bon.delete();
       }
-      await old.delete()
+      await old.delete();
     }
     let res = await Score.create({
-      rider_id: data.rider_id, trial_id: data.trial_id, time: data.time
-    })
+      rider_id: data.rider_id,
+      trial_id: data.trial_id,
+      time: data.time,
+    });
 
-    let pens = []
-    let penaltyTime = 0
+    let pens = [];
+    let penaltyTime = 0;
 
     for (let penalty of data.penalties) {
-      pens.push(await Penalty.create({ ...penalty, score_id: res.id }))
-      const pc = await PenaltyConf.findOrFail(penalty.penalty_conf_id)
-      penaltyTime += (penalty.quantity || 0) * pc.time_penalty
+      pens.push(await Penalty.create({ ...penalty, score_id: res.id }));
+      const pc = await PenaltyConf.findOrFail(penalty.penalty_conf_id);
+      penaltyTime += (penalty.quantity || 0) * pc.time_penalty;
     }
 
-    let bons = []
-    let bonusTime = 0
+    let bons = [];
+    let bonusTime = 0;
 
     for (let bonus of data.bonuses) {
-      bons.push(await Bonus.create({ ...bonus, score_id: res.id }))
-      const bc = await BonusConf.findOrFail(bonus.bonus_conf_id)
-      bonusTime += (bonus.quantity || 0) * bc.time_bonus
+      bons.push(await Bonus.create({ ...bonus, score_id: res.id }));
+      const bc = await BonusConf.findOrFail(bonus.bonus_conf_id);
+      bonusTime += (bonus.quantity || 0) * bc.time_bonus;
     }
 
-    res.time_total = Number(res.time) + Number(penaltyTime) - Number(bonusTime)
-    await res.save()
+    res.time_total = Number(res.time) + Number(penaltyTime) - Number(bonusTime);
+    await res.save();
 
-    res = res.toJSON()
+    res = res.toJSON();
     return response.json({
       updated: true,
-      ...res, penaltyTime, bonusTime,
-      penalties: [...pens], bonuses: [...bons]
-    })
+      ...res,
+      penaltyTime,
+      bonusTime,
+      penalties: [...pens],
+      bonuses: [...bons],
+    });
   }
 }
 
-module.exports = ScoreController
+module.exports = ScoreController;
